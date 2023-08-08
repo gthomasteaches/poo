11dbb75ff8e5ce0ecc3be82a8fbf62f3fd28af2d
Gabriel Thomas <gthomas@udemedellin.edu.co>
2023-08-08 08:27:35 -0500

    Mi segundo commit

--- src/app.py
+++ src/app.py
@@ -5,6 +5,7 @@ mi_naranja = Naranja()
 print("el peso de la naranja es: ", mi_naranja.peso)
 
 mi_canasta = Canasta()
+
 print("Canasta antes",mi_canasta.calcular_cantidad_naranjas())
 mi_naranja.recoger(mi_canasta)
 print("Canasta despues",mi_canasta.calcular_cantidad_naranjas())
